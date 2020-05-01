cdf1621d1920977a8087520fd9b7e63b46e828ed
Hubert Kuc <you@example.com>
2020-05-01 13:01:33 +0200

    WIP

---
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankAIController.h"
+
+
+
+
+void ATankAIController::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    //(LogTemp, Warning, TEXT("Executed TankAIController"));
+}
